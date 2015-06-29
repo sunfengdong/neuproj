c06f89f52caed6da56d442ffe41a013924a3d85b
sfd <sfd@Lab304deiMac-5.local>
2015-06-29 10:37:26 +0800

    Initial Commit

---
+++ neuproj/neuproj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  neuproj
+//
+//  Created by sfd on 15/6/29.
+//  Copyright (c) 2015年 sfd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ neuproj/neuproj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  neuproj
+//
+//  Created by sfd on 15/6/29.
+//  Copyright (c) 2015年 sfd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
